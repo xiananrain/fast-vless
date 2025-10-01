83db67a8b5c4be53fd7df04f4aa327fd648199da
散里散糕 <64390704+Lorry-San@users.noreply.github.com>
2025-10-02 07:59:15 +0800

    继续修复bug

--- xrayvless.sh
+++ xrayvless.sh
@@ -46,9 +46,9 @@ check_and_install_xray() {
   else
     green "❗检测到 Xray 未安装，正在安装..."
 	if [ "$OS" = "alpine" ]; then
-		bash <(curl -L https://lax.xx.kg/https://github.com/Lorry-San/fast-vless/raw/main/xrayinstall-alpine.sh)
+		bash <(curl -L https://lax.xx.kg/https://github.com/Lorry-San/fast-vless/raw/refs/heads/main/xrayinstall-alpine.sh)
 	else
-		bash <(curl -L https://lax.xx.kg/https://github.com/Lorry-San/fast-vless/raw/main/xrayinstall.sh)
+		bash <(curl -L https://lax.xx.kg/https://github.com/Lorry-San/fast-vless/raw/refs/heads/main/xrayinstall.sh)
 	fi
     
     XRAY_BIN=$(command -v xray || echo "/usr/local/bin/xray")
